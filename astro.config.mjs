71fa4a086a06137f19b04e5ab35e7aa166178aa2
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-14 14:10:07 -0600

    set up prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   vite: {
     plugins: [tailwindcss()],
   },
